02a0e2573053d1383b5dd1d39b843fef3547fd3f
lemm0038 <jeff.lemmerman@gmail.com>
2018-10-12 13:27:59 -0500

    Add InMemoryMovieRepository - stores movies in a list

---
+++ NeoMoviesCore.Common/Repositories/InMemoryMovieRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NeoMoviesCore.Common.Model;
+
+namespace NeoMoviesCore.Common.Repositories
+{
+    class InMemoryMovieRepository : IMovieRepository
+    {
+        public IEnumerable<Movie> SearchMoviesByString(string s)
+        {
+            var movieList = new List<Movie>();
+
+            var testMovie = new Movie();
+            testMovie.title = "Movie";
+            testMovie.released = 2000;
+            testMovie.tagline = "Test movie, searched string: " + s;
+
+            movieList.Add(testMovie);
+
+            return movieList;
+        }
+        public IEnumerable<Movie> GetMoviesByTitle(string title)
+        {
+            var movieList = new List<Movie>();
+
+            var testMovie = new Movie();
+            testMovie.title = "Movie";
+            testMovie.released = 2000;
+            testMovie.tagline = "Test movie, searched string: " + title;
+
+            movieList.Add(testMovie);
+
+            return movieList;
+        }
+        public IEnumerable<Movie> GetMoviesByActor(string actor)
+        {
+            var movieList = new List<Movie>();
+
+            var testMovie = new Movie();
+            testMovie.title = "Movie";
+            testMovie.released = 2000;
+            testMovie.tagline = "Test movie, searched string: " + actor;
+
+            movieList.Add(testMovie);
+
+            return movieList;
+        }
+    }
+}
