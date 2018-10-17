99103bd8c653b3dd438330fe3a3bdbd59fc60ef9
Nick <freit039@umn.edu>
2018-10-17 12:16:33 -0500

    Changed Movie Controller class so that a Post on /api/movies will run the CreateMovie method. Tested with the InMemoryMovieRepository, and it was working.

---
+++ NeoMoviesCore/Controllers/MovieController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using NeoMoviesCore.Common.Model;
+using NeoMoviesCore.Common.Repositories;
+
+namespace NeoMoviesCore.Controllers
+{
+    [Route("api/movies")]
+    public class MovieController : Controller
+    {
+        private readonly IMovieRepository _repo;
+
+        public MovieController(IMovieRepository repo)
+        {
+            _repo = repo;
+        }
+
+        // GET: api/movies
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/movies/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/movies
+        [HttpPost]
+        public void Post([FromBody] Movie movie)
+        {
+            _repo.CreateMovie(movie);
+        }
+
+        // AddMovie: api/Movies
+        [HttpPost, Route("api/movies")]
+        public void AddMovie([FromBody] IEnumerable<Movie> movie)
+        {
+            // ???
+            //_repo.AddMovie(movie);
+            
+        }
+
+
+        // PUT: api/Movie/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
+
