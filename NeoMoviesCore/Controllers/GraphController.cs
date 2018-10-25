e01dcc3f28407dc9e4d277ac10af58dd190617ab
sunx0578 <sunx0578@umn.edu>
2018-10-24 21:16:00 -0500

    Represent all the movies and actors in json format

--- NeoMoviesCore/Controllers/GraphController.cs
+++ NeoMoviesCore/Controllers/GraphController.cs
@@ -9,7 +9,7 @@ using Neo4jClient.Cypher;
 using NeoMoviesCore.Common.Model;
 using NeoMoviesCore.Common.Repositories;
 using Newtonsoft.Json;
-
+using Newtonsoft.Json.Linq;
 namespace NeoMoviesCore.Controllers
 {
     [Route("graph")]
@@ -22,17 +22,16 @@ namespace NeoMoviesCore.Controllers
             var user = dbSettings.Value.GraphDBUser;
             var password = dbSettings.Value.GraphDBPassword;
             /*
-			var url = "http://database:7474/db/data";
-			var user = "neo4j"; 
+   var url = "http://database:7474/db/data";
+   var user = "neo4j";
             var password = "movies";
-			*/
+   */
             var client = new GraphClient(new Uri(url), user, password);
             client.Connect();
             GraphClient = client;
         }
         [HttpGet]
         public IActionResult Index()
-
         {
             var query = GraphClient.Cypher
                 .Match("(m:Movie)<-[:ACTED_IN]-(a:Person)")
@@ -41,22 +40,75 @@ namespace NeoMoviesCore.Controllers
                     cast = Return.As<string>("collect(a.name)")
                 });
 
-
-
             //You can see the cypher query here when debugging
-
             var result = query.Results.ToList();
             // Console.WriteLine(data[0]);
-            List<string> data = new List<string>();
-            foreach (var json in result)
+            var nodes = new List<ElementResult>();
+            var edges = new List<EdgeResult>();
+            int i = 0, target, j=0; // j is the id of the edges
+            foreach (var item in result)
             {
-                data.Add(JsonConvert.SerializeObject(json));
+                var data = new Data { id = i.ToString(), name = item.movie, classes = "movie" };
+                nodes.Add(new ElementResult { data = data });
+                target = i; 
+                i++;
+                if (!string.IsNullOrEmpty(item.cast))
+                {
+                    var casts = JsonConvert.DeserializeObject<JArray>(item.cast);
+                    foreach (var cast in casts)
+                    {
+                        var source = nodes.FindIndex(c => c.data.name == cast.Value<string>());
+                        if (source == -1)
+                        {
+                            data = new Data { id = i.ToString(), name = cast.Value<string>(), classes = "actor" };
+                            nodes.Add(new ElementResult { data = data });
+                            source = i;
+                            i += 1;
+                        }
+                        var rel = new Link { id = "e"+j.ToString(), source = source.ToString(), target = target.ToString() };
+                        edges.Add(new EdgeResult{ data = rel});
+                        j += 1;
+                    }
+                }
             }
-            Console.WriteLine(data[0]);
-            System.IO.File.WriteAllLines(@"graph.json", data);
 
-            return Ok();
-
+            List<Object> elements = (from x in nodes select (Object)x).ToList();
+            elements.AddRange((from x in edges select (Object)x).ToList());
+            List<string> text = new List<string>();
+            text.Add(JsonConvert.SerializeObject(new { elements = elements }));
+            System.IO.File.WriteAllLines(@"graph.json", text);
+            //List<string> data = new List<string>();
+            //foreach (var json in result)
+            //{
+            //    data.Add(JsonConvert.SerializeObject(json));
+            //}
+            //Console.WriteLine(data[0]);
+            //System.IO.File.WriteAllLines(@"graph.json", data);
+            return Ok(new { elements });
         }
     }
+    public class Data
+    {
+        public string name { get; set; }
+        public string id { get; set; }
+        public string classes { get; set; }
+    }
+
+    public class Link
+    {
+        public string source { get; set; }
+        public string target { get; set; }
+        public string id { get; set; }
+    }
+
+    public class EdgeResult
+    {
+        public Link data { get; set; }
+    }
+
+    public class ElementResult
+    {
+        public Data data { get; set; }
+    }
 }
+
